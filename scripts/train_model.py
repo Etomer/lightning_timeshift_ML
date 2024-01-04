d9899fc59f564ac1a1292a39f0b2647a15bdbce9
Etomer <erik.tegler@gmail.com>
2024-01-04 10:25:15 +0100

    a

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -6,8 +6,8 @@ import lightning as L
 # choose model and data_module --------------------------------
 from src.data_modules import MovingImpulseResponseDataModule
 from src.cnn_model import cnn_model
-model = cnn_model()
-data_path = "./data/datasets/moving_dataset_small.hdf5"
+model = cnn_model(scale_model_width=10)
+data_path = "./data/datasets/moving_dataset_medium.hdf5"
 sound_dir = "./data/reference_data/reference_sounds/"
 data_module = MovingImpulseResponseDataModule(data_path,sound_dir,batch_size=20)
 #-----------------------------------------------
@@ -18,6 +18,6 @@ trainer = L.Trainer(
     accelerator="cuda",
     devices=1,
     logger=wandb_logger,
-    log_every_n_steps=1,
+    log_every_n_steps=50,
 )
 trainer.fit(model, data_module)
