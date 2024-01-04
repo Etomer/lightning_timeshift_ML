e80cc0b2cca0f1a3b72430293cbed4af59caaca4
Etomer <erik.tegler@gmail.com>
2024-01-04 17:03:29 +0100

    fixing logging behaviour

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,23 +1,29 @@
-import os, sys
+import os, sys, torch
 sys.path.append(os.getcwd())
-from pytorch_lightning.loggers import WandbLogger
+from lightning.pytorch.loggers import WandbLogger
 import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint
 
 # choose model and data_module --------------------------------
 from src.data_modules import MovingImpulseResponseDataModule
 from src.cnn_model import cnn_model
-model = cnn_model(scale_model_width=10)
+
+# define model, (from scratch or checkpoint)
+#model = cnn_model(scale_model_width=10) # from scratch
+model = cnn_model.load_from_checkpoint("checkpoints/epoch=86-step=13224.ckpt", scale_model_width=5, map_location=torch.device("cpu")) # from check point
+
 data_path = "./data/datasets/moving_dataset_medium.hdf5"
 sound_dir = "./data/reference_data/reference_sounds/"
 data_module = MovingImpulseResponseDataModule(data_path,sound_dir,batch_size=20)
 #-----------------------------------------------
-
 wandb_logger = WandbLogger(log_model="all")
+checkpoint_callback = ModelCheckpoint(every_n_epochs=50)
 trainer = L.Trainer(
     max_epochs=1000,
     accelerator="cuda",
-    devices=1,
+    devices=[1],
     logger=wandb_logger,
     log_every_n_steps=50,
+    callbacks=[checkpoint_callback],
 )
 trainer.fit(model, data_module)
