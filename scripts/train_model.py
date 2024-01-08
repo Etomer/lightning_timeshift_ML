0203f25c672f2662ca70261aab1931fc64037402
Etomer <erik.tegler@gmail.com>
2024-01-08 13:16:41 +0100

    update to cnn_model_v2 to allow more block layers and change block_layer size

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -7,25 +7,25 @@ from torchvision import transforms
 
 # choose model and data_module --------------------------------
 from src.data_modules import MovingImpulseResponseDataModule
-from src.cnn_model import cnn_model
+from src.cnn_model_v2 import cnn_model_v2
 import src.data_augmentations as data_augmentations
 
 # define model, (from scratch or checkpoint)
-model = cnn_model(scale_model_width=1) # from scratch
+model = cnn_model_v2(scale_cnn_width=2, n_blocks=4, block_width=2000) # from scratch
 #model = cnn_model.load_from_checkpoint("checkpoints/epoch=999-step=304000.ckpt", scale_model_width=2, map_location=torch.device("cpu")) # from check point
 
 #data_path = "./data/datasets/moving_dataset_medium.hdf5"
 data_path = "data/datasets/moving_dataset_directivity_medium.hdf5"
 sound_dir = "./data/reference_data/reference_sounds/"
 transform = transforms.Compose([data_augmentations.doppler_aug(max_rel_v=3), data_augmentations.noise_aug(noise_ratio=0.01)])
-data_module = MovingImpulseResponseDataModule(data_path,sound_dir,batch_size=40, transform=transform, n_mics_per_batch=11)
+data_module = MovingImpulseResponseDataModule(data_path,sound_dir,batch_size=120, transform=transform, n_mics_per_batch=11)
 #-----------------------------------------------
 wandb_logger = WandbLogger(log_model="all")
 checkpoint_callback = ModelCheckpoint(every_n_epochs=50)
 trainer = L.Trainer(
     max_epochs=3000,
     accelerator="cuda",
-    devices=[1],
+    devices=2,#[1],
     logger=wandb_logger,
     log_every_n_steps=50,
     #terminate_on_nan=True,
